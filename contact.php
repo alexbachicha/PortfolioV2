520723ff52558c87821ff336379185dd15f746a6
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-08-28 12:16:28 -0700

    php verification

--- contact.php
+++ contact.php
@@ -1,19 +1,41 @@
 <?php
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $message = $_POST['message'];
-
-    $email_from = 'Portfolio Contact';
-    $email_subject = 'New Message from Portfolio Contact';
-    $email_body = "Name: $name.\n";
-                  "Email: $email.\n";
-                  "Message: $message.\n";
-
-    $to = "agbachicha@outlook.com";
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $email \r\n";
-
-    mail($to,$email-subject,$email_body,$headers);
-
-    header("location: index.html");
+ 
+// Email address verification
+function isEmail($email) {
+    return filter_var($email, FILTER_VALIDATE_EMAIL);
+}
+ 
+if($_POST) {
+    // Email that receivew message
+    $emailTo = 'agbachicha@outlook.com';
+    
+    $name = addslashes(trim($_POST['name']));
+    $email = addslashes(trim($_POST['email']));
+    $subject = addslashes(trim($_POST['subject']));
+    $message = addslashes(trim($_POST['message']));
+ 
+    $array = array('nameMessage' => '','emailMessage' => '', 'subjectMessage' => '', 'messageMessage' => '');
+    
+    if($name == '') {
+        $array['nameMessage'] = 'Empty name!';
+    }
+    if(!isEmail($email)) {
+        $array['emailMessage'] = 'Invalid email!';
+    }
+    if($subject == '') {
+        $array['subjectMessage'] = 'Empty subject!';
+    }
+    if($message == '') {
+        $array['messageMessage'] = 'Empty message!';
+    }
+    if(isEmail($email) && $name != '' && $subject != '' && $message != '') {
+        // Send email
+        $headers = "From: " . $email . " <" . $email . ">" . "\r\n" . "Reply-To: " . $email;
+        mail($emailTo, $subject . " (bootstrap contact form tutorial)", $message, $headers);
+    }
+ 
+    echo json_encode($array);
+ 
+}
+ 
 ?>
