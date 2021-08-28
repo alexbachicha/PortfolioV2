69eac17bea97dd04e7c1042d242cc8bd7bdb0f5f
Alex Bachicha <alexandra.bachicha@hotmail.com>
2021-08-28 12:17:12 -0700

    updated js file with ajax verification for email

--- js/script.js
+++ js/script.js
@@ -8,3 +8,42 @@ if (toastTrigger) {
     toast.show()
     })
 }
+
+jQuery(document).ready(function() {
+    /* Form */
+    $('.contact-form form input[type="text"], .contact-form form textarea').on('focus', function() {
+        $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('input-error');
+    });
+    $('.contact-form form').submit(function(e) {
+        e.preventDefault();
+        $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('input-error');
+        var postdata = $('.contact-form form').serialize();
+
+        $.ajax({
+            type: 'POST',
+            url: 'contact.php',
+            data: postdata,
+            dataType: 'json',
+            success: function(json) {
+                if(json.nameMessage != '') {
+                    $('.contact-form form .name').addClass('input-error');
+                }
+                if(json.emailMessage != '') {
+                    $('.contact-form form .email').addClass('input-error');
+                }
+                if(json.subjectMessage != '') {
+                    $('.contact-form form .subject').addClass('input-error');
+                }
+                if(json.messageMessage != '') {
+                    $('.contact-form form textarea').addClass('input-error');
+                }
+                if(json.nameMessage == '' && json.emailMessage == '' && json.subjectMessage == '' && json.messageMessage == '') {
+                    $('.contact-form form').fadeOut('fast', function() {
+                        $('.contact-form').append('<p>Thanks for contacting me! I will get back to you very soon.</p>');
+                    });
+                }
+            }
+        });
+    });
+     
+});
